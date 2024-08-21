8642e59d0c838ccb71f29f609c08a3e844dcda71
VidhiDesai2 <157659451+VidhiDesai2@users.noreply.github.com>
2024-08-20 20:46:04 -0500

    - chamged motor D

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrivingSample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrivingSample.java
@@ -43,8 +43,8 @@ public class MecanumDrivingSample extends LinearOpMode {
         // when you first test your robot, push the left joystick forward and observe the direction the wheels turn.
         // Reverse the direction (flip FORWARD <-> REVERSE ) of any wheel that runs backward
         // Keep testing until ALL the wheels move the robot forward when you push the left joystick forward.
-        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
         leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
         rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
 
@@ -103,7 +103,7 @@ public class MecanumDrivingSample extends LinearOpMode {
             //      the setDirection() calls above.
             // Once the correct motors move in the correct direction re-comment this code.
 
-  /*          leftFrontPower  = gamepad1.x ? 1.0 : 0.0;  // X gamepad
+      /*      leftFrontPower  = gamepad1.x ? 1.0 : 0.0;  // X gamepad
             leftBackPower   = gamepad1.a ? 1.0 : 0.0;  // A gamepad
             rightFrontPower = gamepad1.y ? 1.0 : 0.0;  // Y gamepad
             rightBackPower  = gamepad1.b ? 1.0 : 0.0;  // B gamepad
